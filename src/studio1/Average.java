cc7e95df5445d53b79dba3d9e9c8352da7a0c31c
batman <batman@localhost>
2022-09-08 12:31:30 -0500

    hello again

---
+++ src/studio1/Average.java
@@ -0,0 +1,29 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Value for x?");
+		int x = in.nextInt();
+		System.out.println("Value for y?");
+		int y = in.nextInt();
+		int[] nums = {x, y,};
+       
+        float sum = 0;
+        int i=0;
+        while(i < nums.length) {
+            sum += nums[i];
+            i++;
+        }
+         
+        float average = (sum / nums.length); 
+         
+        System.out.println("Average : "+average);
+
+	}
+
+}
